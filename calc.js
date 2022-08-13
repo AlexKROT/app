4102c7e6f750b945d65f2b47f056b959cb4382c7
Aliaksandr Krot <Aliaksandrkrot1995@gmail.com>
2022-08-13 20:34:55 +0200

    Test

--- calc.js
+++ calc.js
@@ -1,13 +1,13 @@
 function calc(operator,a,b) {
 switch (operator) {
     case 'add': 
-   return a+b;
+        return a+b;
     case 'multi':
-    return a*b;
-   case 'subtract':
-    return a-b;
+        return a*b;
+    case 'subtract':
+        return a-b;
     default: 
-    return "Error "    
+     return "Error "    
 }
 
 /*
